0b3a317ca796a87665fc2040bbc8926521073c52
Olulan1 <olulani.o@gmail.com>
2025-11-25 18:48:07 +0000

    why

--- Week-1.7/week-7-worksheet/week-7-worksheet/task2/vpet.py
+++ Week-1.7/week-7-worksheet/week-7-worksheet/task2/vpet.py
@@ -4,31 +4,45 @@
 # (3) hunger - the pet's hunger level, default value is 0
 # When an instance of VirtualPet is created, only the name is needed, as a minimum, for the __init__ method
 
+class VirtualPet(): # class name
+    def __init__(self, name="", energy=10, hunger=0): # default args
+        self.name = name
+        self.energy = energy
+        self.hunger = hunger
+    
+    def play(self):
+        if self.energy >= 2: # option validation
+            self.energy-=2
+            self.hunger+=2
+        else:
+            return (f"{self.name} is too tired to play!")
+    
+    def feed(self):
+        self.hunger-=3
+        if self.hunger <0:
+            self.hunger = 0
+            return (f"{self.name} is overfed!") # check for overfeeding, no need to check for underfeeding
 
+    def sleep(self):
+        self.energy+=10
 
+    def __str__(self):
+       return (f"{self.name} has {self.energy} energy points and hunger level {self.hunger}")
+    
+    def __eq__(self, other):
+        if self.name == other.name:
+            if self.energy == other.energy:
+                if self.hunger == other.hunger:
+                    return True
+                else:
+                    return False
+            else:
+                    return False
+        else:
+                    return False
 
-
-# This class has the following methods:
-# (1) play() - If energy<2, report in the format "{name} is too tired to play!".
-#     Otherwise simulate playing by reducing the energy by 2 and increase the hunger by 2.
-# (2) feed() - Simulate feeding the pet and reducing hunger by 3.
-#     If hunger is less than 0, report in the format "{name} is overfed!" and reset hunger to 0.
-# (3) sleep() - let the pet sleep and increase the energy by 10.
-# (4) __str__() - returns the details of the pet in the format "{name} has {energy} energy points and hunger level {hunger}", 
-#     e.g., "Timmy has 100 energy points and hunger level 0"
-# (5) __eq__() - returns True if all attributes are identical, False otherwise
-
-# You should test each method in your code before submission.
-# Check that attributes are modified as expected
-# For example:
-
-''' Tests
-Pet = VirtualPet("Timmy",4,3)
-print(Pet)
+Pet = VirtualPet("Timmy", 2, 3)
 Pet.play()
-print(Pet)
-Pet.feed()
-print(Pet)
-Pet.sleep()
-print(Pet)
-'''
+Pet.play()
+Pet.play()
+Pet.play()
